fcd7a4b16a986c5c7621bf22cf9911341270f28c
Rimuy <kevinntavares@gmail.com>
2021-11-12 15:27:25 -0300

    Add Action tests

--- tests/src/Tests/Actions.spec.ts
+++ tests/src/Tests/Actions.spec.ts
@@ -1,13 +1,53 @@
 /// <reference types="@rbxts/testez/globals" />
 
-import { Context } from "@rbxts/gamejoy";
+import { Context, Actions } from "@rbxts/gamejoy";
 
 export = () => {
 	const ctx = new Context();
 
 	describe("Action", () => {
-		it("should pass", () => {
-			expect(1).to.be.an("number");
+		const { Action } = Actions;
+
+		it("Normal action", () => {
+			const action = new Action("Q");
+			let passed = false;
+
+			ctx.Bind(action, () => {
+				passed = true;
+			});
+
+			action.Triggered.Fire();
+
+			expect(passed).to.equal(true);
+		});
+		it("Double tap", () => {
+			const passed = new Array<boolean>(1);
+			const cancelled = new Array<boolean>(1);
+
+			const action = new Action("Q", {
+				Repeat: 2,
+				Timing: 0.3,
+			});
+
+			ctx.Bind(action, () => {
+				passed.push(true);
+			});
+
+			action.Cancelled.Connect(() => {
+				cancelled.push(true);
+			});
+
+			for (let i = 0; i < 2; i++) {
+				action.Began.Fire(false);
+				task.wait(0.25);
+			}
+
+			action.Began.Fire(false);
+
+			task.wait(0.32);
+
+			expect(passed.size()).to.equal(1);
+			expect(cancelled.size()).to.equal(1);
 		});
 	});
 };
