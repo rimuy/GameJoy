9e6626c1e0a441c4a49ae98f52be9c4229141630
Rimuy <kevinntavares@gmail.com>
2021-11-26 17:15:01 -0300

    Fix type exports

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ export * as TypeChecks from "./Util/TypeChecks";
 export { withKeypadSupport } from "./Util/withKeypadSupport";
 export { withModifierKeys } from "./Util/withModifierKeys";
 
-export * from "./Definitions/Types";
+export { ActionEntry, AnyAction, AxisActionEntry, RawActionEntry } from "./Definitions/Types";
 
 export const VERSION = PKG_VERSION;
