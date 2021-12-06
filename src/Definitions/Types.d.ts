14a072fe7dc258532b7ee72d66bc877df2727893
Rimuy <kevinntavares@gmail.com>
2021-12-06 06:27:27 -0300

    Filter roblox's unused keys types

--- src/Definitions/Types.d.ts
+++ src/Definitions/Types.d.ts
@@ -51,9 +51,107 @@ export type AxisActionLike =
 	| Enum.KeyCode.ButtonL2
 	| Enum.KeyCode.ButtonR2;
 
+export type UnusedKeys =
+	| Enum.KeyCode.World0
+	| Enum.KeyCode.World1
+	| Enum.KeyCode.World2
+	| Enum.KeyCode.World3
+	| Enum.KeyCode.World4
+	| Enum.KeyCode.World5
+	| Enum.KeyCode.World6
+	| Enum.KeyCode.World7
+	| Enum.KeyCode.World8
+	| Enum.KeyCode.World9
+	| Enum.KeyCode.World10
+	| Enum.KeyCode.World11
+	| Enum.KeyCode.World12
+	| Enum.KeyCode.World13
+	| Enum.KeyCode.World14
+	| Enum.KeyCode.World15
+	| Enum.KeyCode.World16
+	| Enum.KeyCode.World17
+	| Enum.KeyCode.World18
+	| Enum.KeyCode.World19
+	| Enum.KeyCode.World20
+	| Enum.KeyCode.World21
+	| Enum.KeyCode.World22
+	| Enum.KeyCode.World23
+	| Enum.KeyCode.World24
+	| Enum.KeyCode.World25
+	| Enum.KeyCode.World26
+	| Enum.KeyCode.World27
+	| Enum.KeyCode.World28
+	| Enum.KeyCode.World29
+	| Enum.KeyCode.World30
+	| Enum.KeyCode.World31
+	| Enum.KeyCode.World32
+	| Enum.KeyCode.World33
+	| Enum.KeyCode.World34
+	| Enum.KeyCode.World35
+	| Enum.KeyCode.World36
+	| Enum.KeyCode.World37
+	| Enum.KeyCode.World38
+	| Enum.KeyCode.World39
+	| Enum.KeyCode.World40
+	| Enum.KeyCode.World41
+	| Enum.KeyCode.World42
+	| Enum.KeyCode.World43
+	| Enum.KeyCode.World44
+	| Enum.KeyCode.World45
+	| Enum.KeyCode.World46
+	| Enum.KeyCode.World47
+	| Enum.KeyCode.World48
+	| Enum.KeyCode.World49
+	| Enum.KeyCode.World50
+	| Enum.KeyCode.World51
+	| Enum.KeyCode.World52
+	| Enum.KeyCode.World53
+	| Enum.KeyCode.World54
+	| Enum.KeyCode.World55
+	| Enum.KeyCode.World56
+	| Enum.KeyCode.World57
+	| Enum.KeyCode.World58
+	| Enum.KeyCode.World59
+	| Enum.KeyCode.World60
+	| Enum.KeyCode.World61
+	| Enum.KeyCode.World62
+	| Enum.KeyCode.World63
+	| Enum.KeyCode.World64
+	| Enum.KeyCode.World65
+	| Enum.KeyCode.World66
+	| Enum.KeyCode.World67
+	| Enum.KeyCode.World68
+	| Enum.KeyCode.World69
+	| Enum.KeyCode.World70
+	| Enum.KeyCode.World71
+	| Enum.KeyCode.World72
+	| Enum.KeyCode.World73
+	| Enum.KeyCode.World74
+	| Enum.KeyCode.World75
+	| Enum.KeyCode.World76
+	| Enum.KeyCode.World77
+	| Enum.KeyCode.World78
+	| Enum.KeyCode.World79
+	| Enum.KeyCode.World80
+	| Enum.KeyCode.World81
+	| Enum.KeyCode.World82
+	| Enum.KeyCode.World83
+	| Enum.KeyCode.World84
+	| Enum.KeyCode.World85
+	| Enum.KeyCode.World86
+	| Enum.KeyCode.World87
+	| Enum.KeyCode.World88
+	| Enum.KeyCode.World89
+	| Enum.KeyCode.World90
+	| Enum.KeyCode.World91
+	| Enum.KeyCode.World92
+	| Enum.KeyCode.World93
+	| Enum.KeyCode.World94
+	| Enum.KeyCode.World95;
+
 export type AxisActionEntry = CastsToEnum<AxisActionLike>;
 
-export type RawActionLike = Enum.KeyCode | Enum.UserInputType;
+export type RawActionLike = Exclude<Enum.KeyCode | Enum.UserInputType, UnusedKeys>;
 
 export type RawActionEntry = CastsToEnum<RawActionLike> | AliasKey;
 
